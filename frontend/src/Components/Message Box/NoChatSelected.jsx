448ff49fbc00c4c5f61f88e631b23ddb209e44dc
Vivek Shankar Phadake <129990463+Vivek20sp@users.noreply.github.com>
2024-04-23 23:08:24 +0530

    Sixth commit

--- frontend/src/Components/Message Box/NoChatSelected.jsx
+++ frontend/src/Components/Message Box/NoChatSelected.jsx
@@ -5,13 +5,17 @@ import Context from '../../Context/ContextApi';
 const NoChatSelected = () => {
     const context = useContext(Context);
     const { loading, userInfo } = context;
+    let userId;
+    if (!loading && userInfo !== '') {
+        userId = userInfo.user.fullname;
+    }
 
     return (
         <>
             <div className="flex flex-col items-center justify-center text-white md:min-w-[450px] rounded-r-lg shadow-md bg-clip-padding border-l border-gray-100">
                 {
                     (loading && userInfo === '') ? <span className='loading loading-spinner'></span> : <div>
-                        <h1>Welcome 👋 {userInfo.user.fullname} ❄️ </h1>
+                        <h1>Welcome 👋 {userId} ❄️ </h1>
                         <h3>Select  a chat to get started</h3>
                         <HiOutlineChatAlt2 className='text-3xl md:text-6xl text-center' />
                     </div>
